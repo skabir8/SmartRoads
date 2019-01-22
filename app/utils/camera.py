3fe9da123b763df848933e4f72a548712e757722
skabir8 <skabir@stuy.edu>
2019-01-21 21:55:33 -0500

    instantiated machine learning model in init method

--- app/utils/camera.py
+++ app/utils/camera.py
@@ -18,7 +18,11 @@ class VideoCamera(object):
         with open(self.config_path) as config_buffer:
             self.config = json.load(config_buffer)
 
-
+        yolo = YOLO(backend             = self.config['model']['backend'],
+                    input_size          = self.config['model']['input_size'],
+                    labels              = self.config['model']['labels'],
+                    max_box_per_image   = self.config['model']['max_box_per_image'],
+                    anchors             = self.config['model']['anchors'])
         # Initialize video recording environment
         self.is_record = False
         self.out = None
@@ -30,11 +34,7 @@ class VideoCamera(object):
         self.cap.release()
 
     def get_frame(self):
-        yolo = YOLO(backend             = self.config['model']['backend'],
-                    input_size          = self.config['model']['input_size'],
-                    labels              = self.config['model']['labels'],
-                    max_box_per_image   = self.config['model']['max_box_per_image'],
-                    anchors             = self.config['model']['anchors'])
+
         ret, frame = self.cap.read()
 
         if ret:
